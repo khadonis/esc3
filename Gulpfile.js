40290b18997edb17e4d9f93dbbd030c92897fd3b
mustafa kaan demirkol <kaan.demirkol@outlook.com>
2017-02-07 10:52:00 +0300

    07.02.2017

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,43 +1,70 @@
-var gulp = require('gulp');  
-var clean = require('gulp-clean');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var projectError= require('gulp-util');
-var rename = require('gulp-rename');
-var watch = require('gulp-watch');
-var compass = require('gulp-compass'),
-  path = require('path');
-  var cleanCSS = require('gulp-clean-css'); 
+var gulp = require('gulp'),
+clean = require('gulp-clean'),
+concat = require('gulp-concat'),
+uglify = require('gulp-uglify'),
+projectError= require('gulp-util'),
+rename = require('gulp-rename'),
+watch = require('gulp-watch'),
+compass = require('gulp-compass'),
+path = require('path'),
+cleanCSS = require('gulp-clean-css'),
+template = require('gulp-template-html'),
+connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'C:/WORK/THEESCAPEHOTEL/WEBSITE/WEB/escape3',
+    livereload: true,
+    port: 8001
+  });
+});
+
+
+gulp.task('temp', function () {
+    return gulp.src('*.html')
+      .pipe(template('templates/template.html'))
+      .pipe(connect.reload())
+      .pipe(gulp.dest('dist'));      
+});
+
 
 gulp.task('compass', function() {
-  gulp.src('./sass/*.scss')
+  gulp.src('./assets/sass/*.scss')
     .pipe(compass({
       config_file: './config.rb',
       css: 'css',
-      sass: 'sass'
+      sass: 'assets/sass'
     }))
-    .pipe(gulp.dest('app/assets/temp'));
+    .pipe(connect.reload());
 });
 
 gulp.task('minifyCss', function() {
-  return gulp.src('css/custom.css')
-    .pipe(cleanCSS())
-    .pipe(rename('custom.min.css'))
-    .pipe(gulp.dest('css'));
+  return gulp.src('./css/custom.css')
+  .pipe(cleanCSS())
+  .pipe(rename('custom.min.css'))
+  .pipe(gulp.dest('css')).on('error', projectError.log);
 });
 
- 
+/*gulp.task('minifyCssB', function() {
+  return gulp.src('./css/bootstrap.css')
+  .pipe(cleanCSS())
+  .pipe(rename('bootstrap.min.css'))
+  .pipe(gulp.dest('css')).on('error', projectError.log);
+});*/
+
+
 gulp.task('manage', function() {  
-  return gulp.src('js/*.js')
-    .pipe(concat('main.js'))
-    .pipe(uglify())
-    .pipe(rename('main.min.js'))
-    .pipe(gulp.dest('build'))
-    .on('error', projectError.log)
+  return gulp.src('./assets/js/*.js')
+  .pipe(concat('escape.js'))
+  .pipe(uglify())
+  .pipe(rename('escape.min.js'))
+  .pipe(gulp.dest('js'))
+  .on('error', projectError.log)
 });
 
-gulp.task('default', ['compass', 'minifyCss' 'manage'], function() {
-    gulp.watch(['sass/*.scss', 'css/*.css'], ['compass', 'minifyCss']);
-    gulp.watch(['js/*.js', 'js/*.js'], ['manage']);
+gulp.task('default', ['compass', 'minifyCss', 'manage', 'temp', 'connect'], function() {
+  gulp.watch(['assets/sass/*.scss', 'css/*.css'], ['compass', 'minifyCss']);
+  gulp.watch(['assets/js/*.js', 'js/*.js'], ['manage']);
+  gulp.watch(['*.html', 'templates/template.html'], ['temp']);
 });
- 
+
